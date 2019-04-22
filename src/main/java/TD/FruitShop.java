2378e518e14d8ce57ef6a0cac17225274091d8e5
TD-user <td87td87@gmail.com>
2019-04-22 21:32:56 +0300

    update 4

--- src/main/java/TD/FruitShop.java
+++ src/main/java/TD/FruitShop.java
@@ -1,6 +1,7 @@
 package TD;
 
 import GV.Fruit;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,13 +9,32 @@ import java.util.List;
 public class FruitShop {
 
     public List<Fruit> fruits = new ArrayList<Fruit>();
+    private Double moneyBalance;
+    Gson gson = new Gson();
 
     public FruitShop() {
-
+        addFruits("Files\\Delivery");
+        
     }
 
     public void addFruits(String pathToJsonFile) {
-        
+        class Delivery {
+            Fruit[] fruits;
+        }
+
+        try {
+            String json = FileLoader.loadString(pathToJsonFile);
+            System.out.println(json);
+            Delivery delivery = gson.fromJson(json, Delivery.class);
+            if(delivery != null && delivery.fruits != null) {
+                for (Fruit f : delivery.fruits) {
+                    this.fruits.add(f);
+                }
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 
