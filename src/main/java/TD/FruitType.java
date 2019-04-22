b124e5e228928829877692e6554ff84dbc7899e2
TD-user <td87td87@gmail.com>
2019-04-23 00:00:25 +0300

    update enum

--- src/main/java/TD/FruitType.java
+++ src/main/java/TD/FruitType.java
@@ -10,5 +10,15 @@ public enum FruitType {
     Orange,
     Peaches,
     Pear,
-    Pineapple
+    Pineapple;
+
+    public static FruitType getByIndex(int index) throws Exception {
+        return FruitType.values()[index];
+    }
+
+    public static void showTypes() {
+        for(int i = 0; i < FruitType.values().length; i++) {
+            System.out.println(i + " - " + FruitType.values()[i]);
+        }
+    }
 }
