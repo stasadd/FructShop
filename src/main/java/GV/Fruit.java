757fecb4e43acb5bb856b6ec59a1affa820ac322
TD-user <td87td87@gmail.com>
2019-04-22 22:29:22 +0300

    update 6

--- src/main/java/GV/Fruit.java
+++ src/main/java/GV/Fruit.java
@@ -2,13 +2,14 @@ package GV;
 
 import TD.FruitType;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Fruit {
     FruitType type;
     int shelfLife;
-    Date date;
-    double price;
+    String date;
+    int price;
 
     public FruitType getType() {
         return type;
@@ -26,29 +27,32 @@ public class Fruit {
         this.shelfLife = shelfLife;
     }
 
-    public Date getDate() {
-        return date;
+    public Date getDate() throws Exception {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        return format.parse(this.date);
     }
 
     public void setDate(Date date) {
-        this.date = date;
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        this.date = format.format(date);
     }
 
-    public double getPrice() {
+    public int getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(int price) {
         this.price = price;
     }
 
     public Fruit() {
     }
 
-    public Fruit(FruitType type, int shelfLife, Date date, double price) {
+    public Fruit(FruitType type, int shelfLife, Date date, int price) {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         this.type = type;
         this.shelfLife = shelfLife;
-        this.date = date;
+        this.date = format.format(date);
         this.price = price;
     }
 }
--- src/main/java/TD/FruitShop.java
+++ src/main/java/TD/FruitShop.java
@@ -2,28 +2,36 @@ package TD;
 
 import GV.Fruit;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class FruitShop {
 
+    public class Delivery {
+        Fruit[] fruits;
+    }
+
     public List<Fruit> fruits = new ArrayList<Fruit>();
+    private String mainFile = "Files\\SHOP.txt";
     private Double moneyBalance;
     Gson gson = new Gson();
 
     public FruitShop() {
-        fruits.add(new Fruit(FruitType.Apple, 15, new Date("10/07/2017"), 100));
-        save("test.txt");
-        //addFruits("Files\\Delivery");
+        fruits.add(new Fruit(FruitType.Apple, 15, new Date(), 100));
+        //save(mainFile);
+        //load(mainFile);
+        addFruits("Files\\Delivery.txt");
+        System.out.println(fruits.size());
+        System.out.println(fruits.get(0).getType());
     }
 
     public void addFruits(String pathToJsonFile) {
-        class Delivery {
-            Fruit[] fruits;
-        }
-
         try {
             String json = FileLoader.loadString(pathToJsonFile);
             System.out.println(json);
@@ -42,8 +50,23 @@ public class FruitShop {
     public void save(String pathToJsonFile) {
         String json = gson.toJson(this.fruits);
         System.out.println(json);
+        try (FileWriter writer = new FileWriter(pathToJsonFile) ) {
+            writer.write(json);
+            writer.flush();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
-
+    public void load(String pathToJsonFile) {
+        try {
+            String json = FileLoader.loadString(pathToJsonFile);
+            Type collectionType = new TypeToken<List<Fruit>>(){}.getType();
+            fruits = gson.fromJson(json, collectionType);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 
